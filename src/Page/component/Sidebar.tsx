bee5a9a5d3df14f874eb7c9541fc14d90ee72988
nhlong23051 <nhlong23051@gmail.com>
2024-03-19 22:02:37 +0700

    update code

--- src/Page/component/Sidebar.tsx
+++ src/Page/component/Sidebar.tsx
@@ -5,10 +5,10 @@ import { NavLink } from 'react-router-dom'
 type Props = {}
 
 export default function Sidebar({ }: Props) {
-  const classActive = 'border-r-4 border-r-blue-500 border my-6 text-blue-500 rounded-md h-[55px] pt-0 max-w-full text-xl cursor-pointer bg-gray-300 flex items-center duration-300'
-  const classNav = 'my-6 rounded-md h-[55px] pt-0 max-w-full text-base cursor-pointer flex items-center duration-300 hover:bg-gray-200'
+  const classActive = 'border-r-4 border-r-blue-500 border my-6 text-blue-500 rounded-md h-[55px] pt-0 max-w-full lg:text-xl text-base cursor-pointer bg-gray-300 flex items-center duration-300'
+  const classNav = 'my-6 rounded-md h-[55px] pt-0 max-w-full lg:text-base cursor-pointer flex items-center duration-300 hover:bg-gray-200'
   return (
-    <div className="flex flex-col relative z-20 left-0 rounded-xl mr-7 top-0 z-100 bg-white h-[80%] shadow2 w-[20%]">
+    <div className="flex flex-col relative z-20 left-0 rounded-xl mr-7 top-0 z-100 bg-white h-[80%] shadow2 w-[30%] xl:w-[20%]">
       <div className='my-5 p-5 '>
         <NavLink to='/' className={({ isActive }) => isActive ? classActive : classNav}>
           <HomeOutlined className='mx-3' />
@@ -37,12 +37,12 @@ export default function Sidebar({ }: Props) {
 
         <hr />
 
-        <NavLink to='/figure4' className={({ isActive }) => isActive ? classActive : classNav}>
+        <NavLink to='/' className={({ isActive }) => isActive ? classNav : classNav}>
           <LineChartOutlined className='mx-3' />
           Setting
         </NavLink>
 
-        <NavLink to='/figure4' className={({ isActive }) => isActive ? classActive : classNav}>
+        <NavLink to='/' className={({ isActive }) => isActive ? classNav : classNav}>
           <LineChartOutlined className='mx-3' />
           Log
         </NavLink>
